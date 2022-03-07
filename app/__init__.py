ba49d9e65f2e4b30ad30766c0ea8929b9b0a4d17
sidny <sidny.silva@outlook.com>
2022-03-07 20:12:50 -0300

    almost

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,91 @@
-# Desenvolva suas rotas aqui
+from http.client import REQUEST_ENTITY_TOO_LARGE
+import os
+from http import HTTPStatus
+from fileinput import filename
+from pathlib import Path
+from werkzeug.security import safe_join
+from app.kenzie.image import get_file_path, upload_files
+from flask import Flask, jsonify, request, safe_join, send_file
+
+app = Flask(__name__)
+
+PATH = os.getenv('FILES_DIRECTORY')
+EXTENSION = os.getenv("ALLOWED_EXTENSIONS").split(',')
+IMAGE_DIRECTORY = os.getenv('IMAGE_DIRECTORY')
+FILES_DIRECTORY = os.getenv('FILES_DIRECTORY')
+ALLOWED_EXTENSIONS = os.getenv('ALLOWED_EXTENSIONS')
+MAX_CONTENT_LENGTH = os.getenv('MAX_CONTENT_LENGTH')
+
+if not os.path.isdir(PATH):
+    os.mkdir(PATH)
+    for item in EXTENSION:
+        file_path = os.path.join(PATH,EXTENSION)
+        os.mkdir(file_path)
+
+@app.post('/upload')
+def upload():
+    size = request.content_length
+    files_list = request.files  
+    if size > int(MAX_CONTENT_LENGTH):
+        return {'msg': 'file too large'} ,HTTPStatus.REQUEST_ENTITY_TOO_LARGE
+    for file in files_list.values():   
+        file_size = file.content_length 
+        print(file_size)
+        filename = file.filename
+        path = os.path.abspath(FILES_DIRECTORY)
+        file_extension = filename.split('.')[1]
+        dir_path = safe_join(path,file_extension)
+        file_path = safe_join(dir_path,filename)
+        allowed = ALLOWED_EXTENSIONS.split(',')
+    
+        if not allowed.__contains__(file_extension):
+            return {'msg':'Some files are not supported'},HTTPStatus.UNSUPPORTED_MEDIA_TYPE  
+
+        if os.path.isfile(file_path):
+            return {'msg':'This file already exist'},HTTPStatus.CONFLICT
+
+        if not os.path.isdir(dir_path):
+            os.mkdir(dir_path)
+
+        upload_files(file,dir_path)
+    return {'msg': "Image uploaded"}, HTTPStatus.CREATED
+
+
+@app.get('/files')
+def list_files():
+    *_,files_list = next(os.walk(IMAGE_DIRECTORY))
+    return {'msg': files_list}, HTTPStatus.OK
+
+@app.get('/files/<extension>')
+def list_files_by_extension(extension):
+    *_,files_list = next(os.walk(IMAGE_DIRECTORY))
+    chosen_format = []
+    if EXTENSION.__contains__(extension):
+        for item in files_list:
+            if item.split('.')[1] == extension:
+                chosen_format.append(item)
+        return {'msg': chosen_format}
+    return {'msg': 'Format not Found'},HTTPStatus.NOT_FOUND
+
+@app.get('/download/<filename>')
+def download(filename: str):
+    extension = filename.split('.')[1]
+    if EXTENSION.__contains__(extension):
+        filepath = get_file_path(filename)
+        return send_file(filepath, as_attachment=True),HTTPStatus.OK
+    return {'msg': 'Format not Found'},HTTPStatus.NOT_FOUND
+    
+
+@app.get('/download-zip')
+def download_dir_as_zip():
+    extension = request.args.get('file_extension')
+    extension_format = request.args.get('file_extension').split('.')[1]
+    path = os.path.join('/tmp')
+    # if EXTENSION.__contains__(extension_format):               
+    #     command = f"zip -q {extension} {path}"
+    #     os.system(command)
+    #     zipped_file = f'{path}/teste.zip'
+    #     return send_file(zipped_file,as_attachment=True),HTTPStatus.OK
+        # return send_file(filepath, as_attachment=True),HTTPStatus.OK
+    return {'msg': 'Format not Found'},HTTPStatus.NOT_FOUND
+
--- app/kenzie/image.py
+++ app/kenzie/image.py
@@ -1,1 +1,22 @@
-# Desenvolva sua lógica de manipulação das imagens aqui
+from http import HTTPStatus
+import os
+from flask import request, safe_join
+from werkzeug.utils import secure_filename
+from datetime import datetime as dt
+
+
+PATH = os.getenv('FILES_DIRECTORY')
+IMAGE_DIRECTORY = os.getenv('IMAGE_DIRECTORY')
+FILES_DIRECTORY = os.getenv('FILES_DIRECTORY')
+
+def get_file_path(filename: str):
+    abs_path = os.path.abspath(IMAGE_DIRECTORY)
+    filepath = safe_join(abs_path,filename)
+    return filepath
+
+
+def upload_files(file,path): 
+    filename = file.filename
+    file_path = safe_join(path,filename)
+    file.save(file_path)
+    return file_path
